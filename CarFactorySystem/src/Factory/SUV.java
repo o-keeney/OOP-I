4f1c6bed93e3a2b12ab6a544fcca964513440595
o-keeney <o-keeney@github.com>
2024-10-05 12:02:18 +0100

    Added SUV car class

--- CarFactorySystem/src/Factory/SUV.java
+++ CarFactorySystem/src/Factory/SUV.java
@@ -1,4 +1,23 @@
 package Factory;
 
-public class SUV {
+final class SUV extends Car
+{
+    private int numberOfSeats;
+
+    public SUV(String make, String model, int year, int numberOfSeats)
+    {
+        super(make, model, year);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getNumberOfSeats()
+    {
+        return numberOfSeats;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return super.toString() + " - Seating Capacity: " + numberOfSeats;
+    }
 }
